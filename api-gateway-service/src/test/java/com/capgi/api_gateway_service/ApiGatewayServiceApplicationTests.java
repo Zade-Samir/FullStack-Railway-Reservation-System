e83d9693d4b646258304354b95fbdd250e4b1410
Samir-Zade <samirzadeofficial@gmail.com>
2025-04-27 17:43:08 +0530

    creation of api gateway

---
+++ api-gateway-service/src/test/java/com/capgi/api_gateway_service/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgi.api_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
